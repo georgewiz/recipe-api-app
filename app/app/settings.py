b07d1cecbcfc9d967eb0858118374fc71ddd74b7
Jorge Rocha <jorgerocha@Jorges-MacBook-Pro.local>
2024-04-14 20:35:48 -0400

    added postgresql confi to run on docker

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,7 +77,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME')
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_PASS'),
+        'PASWORD': os.environ.get('DB_PASS'),
     }
 }
 
